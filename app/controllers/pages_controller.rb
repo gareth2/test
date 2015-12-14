f91b28133c2a57f08c87712eb9b20129646e0054
Gareth Fernandes <gfernandes_6@hotmail.com>
2015-12-14 18:31:06 -0500

    added services page and word cloud, as well as changed the text colour and border colour in the navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,4 +21,8 @@ class PagesController < ApplicationController
 
   end
 
+  def service
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'pages/result'
 
+  get 'pages/service'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
